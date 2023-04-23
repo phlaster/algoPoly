dff3ed38e502e2c25cbf926cc76c886b6ccb2e31
Alex <curjucklloyd@gmail.com>
2023-04-24 02:05:00 +0300

    just a rename

---
+++ Lab_2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Class_RBTree.cpp"
+/*
+    12.	Реализовать операции добавления и удаления в красно-черное
+    дерево со ссылками на предков и фиктивным элементом, на который
+    указывают листья и корень.
+*/
+int main() {
+    RedBlackTree rbTree;
+    std::cout << "Tree created!\n";
+
+    // Заполняем дерево значениями
+    for (int i = 1; i <= 6; i++) {
+        rbTree.addNode(7*(i-1)+i);
+    }
+
+    std::cout << "Tree now filled with values\n\n\n";
+    std::cout << "Now we delete...\n";
+
+    // Пытаемся удалить некоторые значения
+    for (int i = 0; i <= 20; i++) {
+        rbTree.deleteNode(2*i+1);
+    }
+}
