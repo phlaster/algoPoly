7af4ab18a73ff967511c4b8eeffc717d240afb45
Alex <curjucklloyd@gmail.com>
2023-05-09 20:54:50 +0300

    Big rework: * Added comments * Changed variable names for better readability * Comprehensive demonstration in main() * removed namespace std

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -1,110 +1,141 @@
 #include <iostream>
 #include <queue>
-using namespace std;
 
+// Класс узла дерева
 class TreeNode
 {
-    int *keys;
-    int t;
-    TreeNode **C;
-    int n;
-    bool leaf;
+    int *keys; // целочисленный массив значений в узле 
+    int minDeg; // Минимальная степень узла
+    TreeNode **children; // Массив указателей на детей узла
+    int n; // Количество значений узла (длина массива значений)
+    bool isLeaf; // Является ли узел листом
 
 public:
-    TreeNode(int t1, bool leaf1)
+    // Конструктор
+    TreeNode(int _minDeg, bool _isLeaf)
     {
-        t = t1;
-        leaf = leaf1;
-        keys = new int[2 * t - 1];
-        C = new TreeNode *[2 * t];
-        n = 0;
+        minDeg = _minDeg;
+        isLeaf = _isLeaf;
+        keys = new int[2 * minDeg - 1];
+        children = new TreeNode *[2 * minDeg];
+        n = 0; // Созданный узел пока не имеет значений
     }
 
-    void insertNonFull(int k) {
+    // Вставка значения в узел
+    void insertNonFull(int value) {
         int i = n - 1;
-        if (leaf == true) {
-            while (i >= 0 && keys[i] > k) {
+        // Если узел не заполнен, вставка в правильную позицию массива значений
+        if (isLeaf) {
+            while (i >= 0 && keys[i] > value) {
                 keys[i + 1] = keys[i];
                 i--;
             }
-            keys[i + 1] = k;
-            n = n + 1;
-        } else
+            keys[i + 1] = value;
+            n++;
+        }
+
+        // Если заполнен - рекурсивно разделяем узлы потомков,
+        // Вставляем в правильную позицию в получившееся поддерево
+        else
         {
-            while (i >= 0 && keys[i] > k) i--;
-            if (C[i + 1]->n == 2 * t - 1)
+            while (i >= 0 && keys[i] > value) i--;
+            if (children[i + 1]->n == 2 * minDeg - 1)
             {
-                splitChild(i + 1, C[i + 1]);
-                if (keys[i + 1] < k) i++;
+                splitChild(i + 1, children[i + 1]);
+                if (keys[i + 1] < value) i++;
             }
-            C[i + 1]->insertNonFull(k);
+            children[i + 1]->insertNonFull(value);
         }
     }
 
-    void splitChild(int i, TreeNode *y)
+    // Разделяем i-того ребёнка узла на два узла
+    // Первые minDeg-1 значений и указателей на детей остаются с прежним узлом
+    // Остальные значения и указатели на детей передаются новому узлу newNode
+    void splitChild(int i, TreeNode *node)
     {
-        TreeNode *z = new TreeNode(y->t, y->leaf);
-        z->n = t - 1;
-        for (int j = 0; j < t - 1; j++) z->keys[j] = y->keys[j + t];
-        if (y->leaf == false) for (int j = 0; j < t; j++) z->C[j] = y->C[j + t];
-        y->n = t - 1;
+        TreeNode *newNode = new TreeNode(node->minDeg, node->isLeaf);
+        newNode->n = minDeg - 1;
+        for (int j = 0; j < minDeg - 1; j++) newNode->keys[j] = node->keys[j + minDeg];
+        if (!node->isLeaf) for (int j = 0; j < minDeg; j++) newNode->children[j] = node->children[j + minDeg];
+        node->n = minDeg - 1;
         
-        for (int j = n; j >= i + 1; j--) C[j + 1] = C[j];
-        C[i + 1] = z;
+        for (int j = n; j >= i + 1; j--) children[j + 1] = children[j];
+        // Указатель на i+1-го ребёнка старого узла становится указателем на новый узел
+        children[i + 1] = newNode; 
         
         for (int j = n - 1; j >= i; j--) keys[j + 1] = keys[j];
-        keys[i] = y->keys[t - 1];
+        
+        // i-тое значение старого узла передаётся в подходящую позицию массива значений
+        keys[i] = node->keys[minDeg - 1];
         n++;
     }
 
+    // Объявляем класс-друг, который будет иметь доступ к приватным полям
     friend class BTree;
 };
 
+// Класс b-дерева
 class BTree 
 {
-    TreeNode *root;
-    int t;
+    TreeNode *root; // Указатель на корень дерева
+    int minDeg; // Минимальная степень узла
+    
+    // Проверка заполненности узла
+    bool isFull(TreeNode *node) { return node->n == 2 * minDeg - 1;}
 
 public:
-    BTree(int _t)
+    // Конструктор
+    BTree(int _minDeg)
     {
         root = NULL;
-        t = _t;
+        minDeg = _minDeg;
     }
 
-    void insert(int k) {
+    // Вставляет значение в дерево
+    void insert(int value)
+    {   
+        // Если дерево пустое, создаёт первый узел
         if (root == NULL)
         {
-            root = new TreeNode(t, true);
-            root->keys[0] = k;
+            root = new TreeNode(minDeg, true);
+            root->keys[0] = value;
             root->n = 1;
         }
         else
         {
-            if (root->n == 2 * t - 1)
+            // Если узел полон
+            if (isFull(root))
             {
-                TreeNode *s = new TreeNode(t, false);
-                s->C[0] = root;
+                // Создалим новый узел
+                TreeNode *s = new TreeNode(minDeg, false);
+                s->children[0] = root;
+
+                // Разделим старый надвое
                 s->splitChild(0, root);
                 int i = 0;
-                if (s->keys[0] < k) i++;
-                s->C[i]->insertNonFull(k);
+                if (s->keys[0] < value) i++;
+
+                // И рекурсивно вставим значение в правильное поддерево
+                s->children[i]->insertNonFull(value);
                 root = s;
             }
-            else root->insertNonFull(k);
+            // Если узел не заполнен, то просто вставляем значение
+            else root->insertNonFull(value);
         }
     }
 
+    // Печатаем дерево
     void printLevelOrder()
     {
+        // Для пустого дерева
         if (root == NULL)
         {
-            cout << "Tree is empty!\n";
+            std::cout << "Tree is empty!\n";
             return;
         }
-        queue<TreeNode *> q;
+        // Используем структуру "очередь" из стандартной библиотеки
+        std::queue<TreeNode *> q;
         q.push(root);
-
         while (!q.empty())
         {   
             int n = int(q.size());
@@ -113,15 +144,22 @@ public:
                 TreeNode *node = q.front();
                 q.pop();
 
-                if (!node->leaf) for (int j = 0; j < (20/n)+5; j++) cout << ' ';
+                
+                /*
+                    Не самая чистая работа. Числа подбирались, чтобы более-менее адекватно
+                    Отображать пример из задания и другие деревья на 10-15 элементов
+                */
+                if (!node->isLeaf) for (int j = 0; j < (20-7*n); j++)
+                    std::cout << ' ';
 
-                cout << "(";
-                for (int j = 0; j < node->n; j++) cout << " " << node->keys[j] << " ";
-                cout << ")";
+                std::cout << '(';
+                for (int j = 0; j < node->n; j++)
+                  std::cout << node->keys[j] << ',';
+                std::cout << "\b) "; // Удаляем последнюю запятую
 
-                if (!node->leaf) for (int k = 0; k <= node->n; k++) q.push(node->C[k]);
+                if (!node->isLeaf) for (int k = 0; k <= node->n; k++) q.push(node->children[k]);
             }
-            cout << endl;
+            std::cout << std::endl;
         }
     }
 };
@@ -142,11 +180,31 @@ int main() {
     t.insert(30);
     t.insert(33);
     t.insert(40);
-    cout << "The B-tree is: \n"; t.printLevelOrder();
-    cout << endl;
+    std::cout << "14 The B-tree is: \n";
+    t.printLevelOrder();
+    std::cout << std::endl;
 
     BTree t2(3);
-    for(int i=0; i<40; i++) t2.insert(i);
-    cout << "The B-tree is: \n"; t2.printLevelOrder();
-    cout << endl;
+    for(int i=0; i<10; i++) t2.insert(i);
+    std::cout << "10 The B-tree is: \n";
+    t2.printLevelOrder();
+    std::cout << std::endl;
+
+    BTree t3(3);
+    for(int i=0; i<12; i++) t3.insert(i);
+    std::cout << "12 The B-tree is: \n";
+    t3.printLevelOrder();
+    std::cout << std::endl;
+
+    BTree t4(3);
+    for(int i=0; i<15; i++) t4.insert(i);
+    std::cout << "15 The B-tree is: \n";
+    t4.printLevelOrder();
+    std::cout << std::endl;
+
+    BTree t5(3);
+    for(int i=0; i<20; i++) t5.insert(i);
+    std::cout << "20 The B-tree is: \n";
+    t5.printLevelOrder();
+    std::cout << std::endl;
 }
