7181d80c0326357b554b7eaff349882c0721cf47
Alex <curjucklloyd@gmail.com>
2023-06-12 09:09:05 +0300

    Enhancements and reorderring (still in process) Commit before reorganisation of methods

--- Lab_4/Huffman.cpp
+++ Lab_4/Huffman.cpp
@@ -2,24 +2,106 @@
 #include <iostream>
 #include <queue>
 #include <unordered_map>
-#include "Node.hpp"
+#include <bitset>
+#include "Types.hpp"
+#include "HuffmanPrinter.cpp"
 #include <iomanip>
-
-typedef std::string Str;
-typedef std::unordered_map<char, int> UMapFrT;
-typedef std::unordered_map<char, Str> UMapCode;
-
 
 class HuffmanCoding
 {
+private:
+    friend HuffmanPrinter;
+    static Str bitBuffer;
+
+    static Node *buildHuffmanTree(const std ::unordered_map<char, int> &freqTable)
+    {
+
+        auto compare = [](const Node *lhs, const Node *rhs)
+        {
+            return *lhs > *rhs;
+        };
+
+        std::priority_queue<Node *, std::vector<Node *>, decltype(compare)>
+            minHeap(compare);
+
+        for (auto f : freqTable)
+        {
+            // std::cout << f.first << " - " << f.second << "\n"; // debug
+            Node *temp = new Node{f.first, f.second, nullptr, nullptr};
+            minHeap.push(temp);
+        }
+
+        while (minHeap.size() > 1)
+        {
+            Node *left = minHeap.top();
+            minHeap.pop();
+
+            Node *right = minHeap.top();
+            minHeap.pop();
+
+            int freqSum = left->frequency + right->frequency;
+
+            Node *parentNode = new Node{'\0', freqSum, left, right};
+
+            minHeap.push(parentNode);
+        }
+
+        Node *root = minHeap.top();
+        minHeap.pop();
+
+        return root;
+    }
+
+    static UMapCode generateCodes(Node *root, Str code)
+    {
+        UMapCode codeMap;
+
+        if (root == nullptr)
+        {
+            return codeMap;
+        }
+
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            codeMap[root->data] = code;
+            return codeMap;
+        }
+
+        UMapCode leftMap = generateCodes(root->left, code + "0");
+        UMapCode rightMap = generateCodes(root->right, code + "1");
+
+        codeMap.insert(leftMap.begin(), leftMap.end());
+        codeMap.insert(rightMap.begin(), rightMap.end());
+
+        return codeMap;
+    }
+
+    static void writeByte(std::ofstream &outputFile)
+    {
+        unsigned char byte = std::bitset<8>(bitBuffer).to_ulong();
+        outputFile.put(byte);
+        bitBuffer.clear();
+    }
+
+    static void writeSingleCharacterFile(Node *root, const Str &outputFilePath)
+    {
+        std::ofstream outputFile(outputFilePath, std::ios_base::out | std::ios_base::binary);
+        if (!outputFile.is_open())
+            throw std::invalid_argument("Failed to open output file");
+
+        unsigned char byte = (unsigned char)root->data;
+        outputFile.write(reinterpret_cast<char *>(&byte), sizeof(byte));
+
+        outputFile.close();
+    }
+    
 public:
-    static void
-    compress(const Str &inputFilePath,
-             const Str &outputFilePath) {
-        
+    static void compress(const Str &inputFilePath, const Str &outputFilePath)
+    {
         // Step 1: Read input file
         std::ifstream inputFile(inputFilePath);
-        if (!inputFile.is_open()) throw std::invalid_argument("Failed to open input file");
+        if (!inputFile.is_open())
+            throw std::invalid_argument("Failed to open input file");
 
         // Count frequencies of each character in the text
         UMapFrT freqTable;
@@ -31,25 +113,34 @@ public:
             c = inputFile.get();
         }
 
-        if (c != EOF) ++freqTable[c];
+        if (c != EOF)
+            ++freqTable[c];
 
-        printFreq(freqTable);
+        HuffmanPrinter::printFreq(freqTable);
 
         // Step 2: Build Huffman Tree
         Node *root = buildHuffmanTree(freqTable);
-        printTree(root); // debug
+        HuffmanPrinter::printTree(root); // debug
 
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            // Single character input file
+            writeSingleCharacterFile(root, outputFilePath);
+            inputFile.close();
+            return;
+        }
 
         // Step 3: Generate bit codes for each symbol
         UMapCode codeMap = generateCodes(root, "");
-        printCodes(codeMap);
+        HuffmanPrinter::printCodes(codeMap);
 
         // Write compressed data to output file
         inputFile.clear(); // clear fail and eof bits
-        inputFile.seekg(0, std ::ios_base ::beg);
+        inputFile.seekg(0, std::ios_base::beg);
         std::ofstream outputFile(outputFilePath, std::ios_base::out | std::ios_base::binary);
 
-        if (!outputFile.is_open()) throw std::invalid_argument("Failed to open output file");
+        if (!outputFile.is_open())
+            throw std::invalid_argument("Failed to open output file");
 
         char symbol = inputFile.get();
         while (inputFile.good())
@@ -58,12 +149,11 @@ public:
             for (auto c : code)
             {
                 bitBuffer += Str(1, c);
-                if (bitBuffer.size() == 8) writeByte(outputFile);
+                if (bitBuffer.size() == 8)
+                    writeByte(outputFile);
             }
             symbol = inputFile.get();
         }
-
-
 
         // Write any remaining bits to output file
         if (bitBuffer.size() > 0)
@@ -79,20 +169,20 @@ public:
         outputFile.close();
     }
 
-    static void
-    decompress(const Str &inputFilePath,
-               const Str &outputFilePath){
+    static void decompress(const Str &inputFilePath, const Str &outputFilePath)
+    {
         int numOfSymbols;
         unsigned char header[256];
         std::ifstream inputFile(inputFilePath, std::ios_base::in | std::ios_base::binary);
-
-        if (!inputFile.is_open()) throw std::invalid_argument("Failed to open input file");
+        if (!inputFile.is_open())
+            throw std::invalid_argument("Failed to open input file");
 
         unsigned char c;
         for (int i = 0; i <= 255 && !inputFile.eof(); ++i)
         {
             inputFile.read(reinterpret_cast<char *>(&c), sizeof(c));
             header[i] = c;
+            std::cout << c << "\n";
         }
 
         numOfSymbols = header[255];
@@ -108,20 +198,19 @@ public:
         Node *root = buildHuffmanTree(freqTable);
         std::ofstream outputFile(outputFilePath);
 
-        if (!outputFile.is_open()) throw std::invalid_argument("Failed to open output file");
+        if (!outputFile.is_open())
+            throw std::invalid_argument("Failed to open output file");
 
-
-        for(int i=0; i<10; i++)
+        for (int i = 0; i < 10; i++)
         {
-            unsigned char x; 
-            inputFile.read(reinterpret_cast<char*>(&x),sizeof(x));
-            std::cout << x <<"\n";
+            unsigned char x;
+            inputFile.read(reinterpret_cast<char *>(&x), sizeof(x));
+            std::cout << x << "\n";
         }
-
 
         Node *current = root;
 
-        while (!inputFile.eof() && numOfSymbols > 0)
+        while (numOfSymbols > 0)
         {
             unsigned char byte;
             inputFile.read(reinterpret_cast<char *>(&byte), sizeof(byte));
@@ -133,7 +222,6 @@ public:
                 if (bit == false)
                 {
                     current = current->left;
-
                     if (current->data != '\0') // Found a symbol
                     {
                         outputFile << current->data;
@@ -144,7 +232,6 @@ public:
                 else
                 {
                     current = current->right;
-
                     if (current->data != '\0') // Found a symbol
                     {
                         outputFile << current->data;
@@ -155,104 +242,9 @@ public:
             }
         }
 
-        inputFile.close();
-        outputFile.close();
-    }
-
-private:
-    static Str bitBuffer;
-
-    static Node *
-    buildHuffmanTree(const std ::unordered_map<char, int> &freqTable) {
-
-        auto compare = [](const Node *lhs, const Node *rhs)
+        if (numOfSymbols != 0)
         {
-            return *lhs > *rhs;
-        };
-
-        std::priority_queue<Node *, std::vector<Node *>, decltype(compare)>
-            minHeap(compare);
-
-        for (auto f : freqTable)
-        {
-            std::cout << f.first << " - " << f.second << "\n"; // debug
-            Node *temp = new Node{f.first, f.second, nullptr, nullptr};
-            minHeap.push(temp);
-        }
-
-        while (minHeap.size() > 1)
-        {
-            auto left = minHeap.top();
-            minHeap.pop();
-            auto right = minHeap.top();
-            minHeap.pop();
-
-            int sumFreq = left->frequency + right->frequency;
-
-            Node *parent = new Node{'\0', sumFreq, left, right};
-            minHeap.push(parent);
-        }
-        return minHeap.top();
-    }
-
-    static UMapCode
-    generateCodes(Node *root, const Str &code) {
-        UMapCode result;
-
-        if (root->left != nullptr)
-            generateCodes(root->left, code + "0");
-        if (root->right != nullptr)
-            generateCodes(root->right, code + "1");
-        if (root->data != '\0')
-            result[root->data] = code;
-
-        return result;
-    }
-
-    static void
-    writeByte(std::ofstream &outputFile) {
-        unsigned char byte = 0;
-        for (int i = 7; i >= 0; --i) // writing bits from the buffer as bytes
-        {
-            bool bit = bitBuffer[i] - '0';
-            byte |= bit << i;
-        }
-
-        outputFile.write(reinterpret_cast<const char *>(&byte), sizeof(byte));
-        bitBuffer.clear();
-    }
-
-    static void
-    printTree(Node* root, int indent = 0){
-        if (root != nullptr)
-        {
-            printTree(root->right, indent + 4);
-            std::cout << std::setw(indent) << ' ';
-            std::cout << root->data << ":" << root->frequency << std::endl;
-            printTree(root->left, indent + 4);
+            throw std::invalid_argument("Output file is incomplete.");
         }
     }
-
-    static void
-    printCodes(UMapCode codeMap) {
-        std::cout<<"Printing Codes"<< "\n";
-        for(auto i : codeMap)
-            std::cout <<
-            i.first <<
-            "\t" <<
-            i.second <<
-            "\n";
-    }
-    
-    static void
-    printFreq(UMapFrT freqTable) {
-        for(auto it : freqTable)
-            std::cout <<
-            "Symbol: " <<
-            it.first <<
-            ", Frequency: " <<
-            it.second <<
-            std::endl;
-    }
-
 };
